d8f6a6f79e6fb5a21e7487df7efe59681b968b5f
tefwar <tefagrimaldo@gmail.com>
2022-05-17 18:09:22 -0500

    New tokens added and implemented

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import MainColor from "./functions/tokens";
-
+import {BGColor, MainColor, UiBorder, PrimaryText, BorderRadius,PositiveColor, NegativeColor, WarningColor,} from "./functions/tokens";
+import { SmileySad, SmileyMeh, Smiley } from "phosphor-react";
 
 const App = () => {
-  let color = MainColor();
-
   return (
     <div className="App">
-      <header className="App-header">
-        <h3 className="App-h3">Califica tu servicio</h3>
-        <h6 className="App-h6">Cuéntanos cómo fue tu experiencia</h6>
-        <textarea className="App-textarea"></textarea>
-        <button style={{ background: color ?? "#fff" }} className="App-button">Guardar</button>
-        <p style={{ color: color ?? "#fff" }}>Color change variable</p>
-        
+      <header style={{ background: PrimaryText() ?? "#fff" }} className="App-header">
+        <div style={{ background: BGColor() ?? "#fff", borderRadius :BorderRadius()+"px"}} styleclassName="App-inside">
+        <h3 style={{ color: PrimaryText() ?? "#fff" }} className="App-h3">Califica tu servicio</h3>
+        <h6  style={{ color: PrimaryText() ?? "#fff" }} className="App-h6">Cuéntanos cómo fue tu experiencia</h6>
+          <div>
+            <SmileySad color={NegativeColor() ?? "#fff" } size={48} weight="bold"/>
+            <SmileyMeh color={WarningColor() ?? "#fff" } size={48} weight="bold"/>
+            <Smiley color={PositiveColor() ?? "#fff" } size={48} weight="bold"/>
+          </div>
+        <textarea style={{ borderColor: UiBorder() ?? "#fff" }} className="App-textarea"></textarea>
+        <button style={{ background: MainColor() ?? "#fff" }} className="App-button">Guardar</button>
+        <p style={{ color: MainColor() ?? "#fff" }}>Color change variable</p>
+        </div>
       </header>
     </div>
     
